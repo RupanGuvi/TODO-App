b799b8e9793f65d5b63b3c5115d32a78cfec6c2c
RupanGuvi <rupanguvi@gmail.com>
2024-09-17 18:47:01 +0530

    App completed

--- script.js
+++ script.js
@@ -62,8 +62,10 @@ const createTask = () => {
               <span class="fw-bolder">${ele.date}</span>
                <p class="fw-bold">${ele.task}</p>
                <span class="options">
-               <i class="fa-solid fa-pen-to-square fa-beat fa-lg" style="color: #FFD43B;"></i>
-               <i class="fa-solid fa-trash fa-beat fa-lg" style="color: #FFD43B;"></i>
+               <i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square fa-beat fa-lg" style="color: #FFD43B;"></i>
+               <i onclick="deleteTask(this); createTask()" class="fa-solid fa-trash fa-beat fa-lg" style="color: #FFD43B;"></i>
+               </span>
+               </div>
             `);
   });
   resetForm();
@@ -82,4 +84,25 @@ const resetForm =() =>{
     createTask();
     
 }) ();
+
+//Edit function for Created TODO's
+
+const editTask = (e) =>{
+     let result = e.parentElement.parentElement;
+     textInput.value = result.children[0].innerHTML;
+     dateInput.value = result.children[1].innerHTML;
+     textarea.value = result.children[2].innerHTML;
+
+     //to remove the old task after edited
+     deleteTask(e);
+}
+
+// delete function for created TODO's
+
+const deleteTask = (e)=>{
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id,1);
+  localStorage.setItem("data",JSON.stringify(data));
+  //console.log(data);
+}
 
